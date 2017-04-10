24270e725d649ce6e464094422c15f9006c02a14
Siddharth Sharma <siddharthsharma819@gmail.com>
2017-04-10 22:50:20 +0530

    DFS with stack
    Depth First Search of graph with stack implementation.

---
+++ DFS_Stack.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Graph
+{
+    int v;
+    ArrayList al[];
+    Graph(int v)
+    {
+        this.v=v;
+        al=new ArrayList[v];
+        for(int i=0;i<v;i++)
+            al[i]=new ArrayList<Integer>();
+    }
+    void addEdge(int x,int y)
+    {
+        al[x-1].add(y-1);
+    }
+    void dfs()
+    {
+        boolean visited[]=new boolean[this.v];
+        for(int i=0;i<this.v;i++)
+        {
+            if(!visited[i])
+            {
+                dfsTraverse(i,visited);
+            }
+        }
+        System.out.println();
+    }
+    void dfsTraverse(int i,boolean visited[])
+    {
+        Stack<Integer> stk=new Stack<Integer>();
+        stk.push(i);
+        while(!stk.empty())
+        {
+            int a=stk.pop();
+            if(!visited[a])
+            {
+                visited[a]=true;
+                System.out.print(""+a+' ');
+            }
+            Iterator<Integer> itr=al[a].iterator();
+            while(itr.hasNext())
+                stk.push(itr.next());
+        }
+    }
+    void bfs(int v)
+    {
+        if(v>this.v)
+        {
+            System.out.println("Invalid starting vertex");
+            return;
+        }
+        boolean visited[]=new boolean[this.v];
+        LinkedList<Integer> q=new LinkedList<Integer>();
+        visited[v-1]=true;
+        q.add(v-1);
+        while(q.size()>0)
+        {
+            int a=q.poll();
+            System.out.print(""+a+' ');
+            Iterator<Integer> itr=al[a].iterator();
+            while(itr.hasNext())
+            {
+                int b=itr.next();
+                if(!visited[b])
+                {
+                    visited[b]=true;
+                    q.add(b);
+                    
+                }
+            }
+        }
+        System.out.println();
+    }
+}
+class Test
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Graph g=new Graph(4);
+	    g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(2,4 );
+ 
+        System.out.println("Following is Breadth First Traversal "+
+                           "(starting from vertex 1)");
+ 
+        g.bfs(1);
+        System.out.println("Following is Depth First Traversal "+
+                           "(starting from vertex 1)");
+ 
+        g.dfs();
+	}
+}
